195a0e630615d46d0d6893a1b8c914b1710090cc
feel2174 <feel2174@gmail.com>
2018-11-06 23:35:12 +0900

    learning Java

---
+++ zucca/src/zucca/zucca2.java
@@ -0,0 +1,16 @@
+package zucca;
+
+public class zucca2 {
+
+	public static void main(String[] args) {
+		// 이항연산자
+		boolean b1 = false;
+		boolean b2 = true;
+		System.out.println("b1 && b2 : " + (b1 && b2));
+		System.out.println("b1 || b2 : " + (b1 || b2));
+		System.out.println("!b1 : " + !b1);
+		System.out.println("!b2 : " + !b2);
+		
+	}
+
+}
