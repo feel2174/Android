c5451ceb411e6ebe8bfbed8df2a75835ccbc7c5c
feel2174 <37773812+feel2174@users.noreply.github.com>
2019-03-06 22:54:34 +0100

    Add JavasrcFiles from Ubuntu (#1)
    Well done

---
+++ src/StudentComparator.java
@@ -0,0 +1,10 @@
+import java.util.Comparator;
+
+public class StudentComparator implements Comparator<CompareExample.Student> {
+    @Override
+    public int compare(CompareExample.Student a, CompareExample.Student b) {
+        if(a.sno < b.sno) return -1;
+        else if(a.sno == b.sno) return 0;
+        else return 1;
+    }
+}
