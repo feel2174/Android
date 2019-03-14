587a405199e9a39e8126fa080314dafb7fbe30f7
feel2174 <37773812+feel2174@users.noreply.github.com>
2019-03-14 13:50:58 +0100

    Add Fragment (#7)
    * Add Facebook login function
    
    * Add Webview function
    
    * Add Webview
    
    * Add Fragment(About Us)

--- fcm/app/src/main/java/com/example/devzucca/fcm/MainActivity.java
+++ fcm/app/src/main/java/com/example/devzucca/fcm/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.devzucca.fcm;
 
+
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
-
+import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
+
 
 import android.widget.Button;
 import android.widget.EditText;
@@ -25,31 +32,31 @@ import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.google.firebase.auth.FirebaseAuth;
 
-import java.util.Arrays;
+
 
 public class MainActivity extends AppCompatActivity {
     private WebView mWebView;
     private WebSettings mWebSettings;
     private EditText etEmail;
 
-    // 페이스북 콜백 매니저
-    private CallbackManager callbackManager;
-    // 파이어베이스 인증 객체 생성
-    private FirebaseAuth firebaseAuth;
 
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
+    private Button button5;
+    private Button button6;
 
-    private EditText etEmail;
+    private CallbackManager callbackManager; // 페이스북 콜백 매니저
+    private FirebaseAuth firebaseAuth; // 파이어베이스 인증 객체 생성
 
-    // 페이스북 콜백 매니저
-    private CallbackManager callbackManager;
-    // 파이어베이스 인증 객체 생성
-    private FirebaseAuth firebaseAuth;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
 
         callbackManager = CallbackManager.Factory.create();
@@ -68,37 +75,40 @@ public class MainActivity extends AppCompatActivity {
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                // App code
+
             }
 
             @Override
             public void onCancel() {
-                // App code
+
             }
 
             @Override
             public void onError(FacebookException exception) {
-                // App code
+
+
             }
         });
 
-
-        LoginManager.getInstance().registerCallback(callbackManager,
-                new FacebookCallback<LoginResult>() {
-
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
                     @Override
                     public void onSuccess(LoginResult loginResult) {
-                        // App code
+                        Toast.makeText(getApplicationContext(), "로그인을 성공했습니다.", Toast.LENGTH_LONG).show();
+
                     }
 
                     @Override
                     public void onCancel() {
-                        // App code
+
+                        Toast.makeText(getApplicationContext(), "로그인에 실패했습니다.", Toast.LENGTH_LONG).show();
+
                     }
 
                     @Override
                     public void onError(FacebookException exception) {
-                        // App code
+
+                        Toast.makeText(getApplicationContext(), "로그인중 오류가 발생했습니다.", Toast.LENGTH_LONG).show();
+
                     }
                 });
 
@@ -113,53 +123,76 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button button1 = (Button) findViewById(R.id.button);
+
+        button1 = (Button) findViewById(R.id.button);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/events/2531087493585497/"));
+                Intent intent = new Intent(MainActivity.this, Webview.class);
                 startActivity(intent);
             }
         });
-        Button button2 = (Button) findViewById(R.id.button2);
-
+        button2 = (Button) findViewById(R.id.button2);
         button2.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/events/329395127783194/"));
+                Intent intent = new Intent(MainActivity.this, Webview2.class);
                 startActivity(intent);
             }
         });
-        Button button3 = (Button) findViewById(R.id.button3);
+        button3 = (Button) findViewById(R.id.button3);
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/events/1138881746292105/"));
+                Intent intent = new Intent(MainActivity.this, Webview3.class);
                 startActivity(intent);
             }
         });
-        Button button4 = (Button) findViewById(R.id.button4);
+        button4 = (Button) findViewById(R.id.button4);
         button4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Intent intent = new Intent(MainActivity.this, Webview.class);
+                Intent intent = new Intent(MainActivity.this, Webview4.class);
                 startActivity(intent);
-
 
             }
         });
-
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/events/1872814239512367/"));
+        button5 = (Button) findViewById(R.id.button5);
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
                 startActivity(intent);
             }
         });
+        button6 = (Button)findViewById(R.id.button6);
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main3Activity.class);
+
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+
+
 
 
 
 
     }
+
 
 
     @Override
---
+++ fcm/app/src/main/java/com/example/devzucca/fcm/Webview3.java
@@ -0,0 +1,27 @@
+package com.example.devzucca.fcm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class Webview3 extends AppCompatActivity {
+
+    private WebView mWebView;
+    private WebSettings mWebSettings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview3);
+
+
+        mWebView = (WebView) findViewById(R.id.webview);
+        mWebView.setWebViewClient(new WebViewClient());
+        mWebSettings = mWebView.getSettings();
+        mWebSettings.setJavaScriptEnabled(true);
+        mWebView.loadUrl("https://www.tickettailor.com/events/youngbros/240196");
+
+    }
+}
